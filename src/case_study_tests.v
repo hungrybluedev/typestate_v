ddd578a382f8677482d88a5e490b93e77c85246d
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-07-08 18:21:31 +0100

    Allow cross-platform paths in errors.

--- src/case_study_tests.v
+++ src/case_study_tests.v
@@ -13,6 +13,9 @@ const (
 		// os.join_path('01_read_only_file', 'case04_invalid'),
 		// Speaker
 		os.join_path('02_speaker', 'case01_errors'),
+		os.join_path('02_speaker', 'case02_warnings'),
+		os.join_path('02_speaker', 'case03_normal'),
+		// os.join_path('02_speaker', 'case04_invalid'),
 		// RNG
 	]
 )
@@ -30,7 +33,9 @@ fn run_for_case_studies(command cli.Command) ! {
 
 		start(cli.Command{ args: [path] }) or {
 			// An error occurred. Check to see if it was expected.
-			expected_error := os.read_file(expected_error_path)!
+
+			// To make the errors cross-platform, replace all forward slashes with the OS-specific separator.
+			expected_error := os.read_file(expected_error_path)!.replace('/', os.path_separator)
 			actual_error := err.str()
 
 			if expected_error != actual_error {
