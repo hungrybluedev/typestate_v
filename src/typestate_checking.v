8e6caddb15e107257ff5275c47a9600879c8478c
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-07-23 17:36:34 +0100

    Add return and use match for expressions

--- src/typestate_checking.v
+++ src/typestate_checking.v
@@ -87,6 +87,11 @@ fn (mut context TypestateContext) check_statements(statements []ast.Stmt, fn_fil
 			ast.ForStmt {
 				context.check_statements(statement.stmts, fn_file)!
 			}
+			ast.Return {
+				for expr in statement.exprs {
+					context.check_expression(expr, fn_file)!
+				}
+			}
 			else {
 				// unsupported statement type
 				println('UNSUPPORTED: ${statement.type_name()}\t${statement}')
@@ -96,21 +101,60 @@ fn (mut context TypestateContext) check_statements(statements []ast.Stmt, fn_fil
 }
 
 fn (mut context TypestateContext) check_expression(expression ast.Expr, fn_file string) ! {
-	if expression is ast.CallExpr
-		&& (expression as ast.CallExpr).left_type == context.target_type.idx {
-		call_expr := expression as ast.CallExpr
-		identifier := (call_expr.left as ast.Ident).name
+	match expression {
+		ast.CallExpr {
+			call_expr := expression as ast.CallExpr
+			if call_expr.left_type == context.target_type.idx {
+				identifier := (call_expr.left as ast.Ident).name
 
-		if identifier !in context.reference_map {
-			return error('Instance of ${context.target_type.name} identified as ${identifier} is not initialized.')
+				if identifier !in context.reference_map {
+					return error('Instance of ${context.target_type.name} identified as ${identifier} is not initialized.')
+				}
+
+				mut automata := context.reference_map[identifier] or {
+					return error('Could not find the automata for ${identifier}.')
+				}
+
+				automata.accept('${context.just_name}.${call_expr.name}') or {
+					return error(serialise_state_error(err, fn_file, call_expr.pos.line_nr))
+				}
+
+				// Check the statements inside the method
+				method := context.target_type.find_method(call_expr.name) or {
+					return error('Could not find the method ${call_expr.name} in ${context.target_type.name}.')
+				}
+				method_name := context.just_name + '.' + method.name
+
+				if method_name !in context.visited_functions {
+					context.visited_functions[method_name] = true
+
+					// Check the receiver type of the method
+					receiver_name := method.params[0].name
+
+					// Add a sub-automata for the receiver
+					sub_automata := automata.clone_ref()
+					context.reference_map[receiver_name] = sub_automata
+
+					context.check_function(method)!
+
+					// Remove the sub-automata
+					context.reference_map.delete(receiver_name)
+				}
+			} else {
+				// It was not a method. Check everything
+				for arg in call_expr.args {
+					context.check_expression(arg.expr, fn_file)!
+				}
+				fn_name := call_expr.name
+				if fn_name in context.builder.table.fns && fn_name !in context.visited_functions {
+					context.visited_functions[fn_name] = true
+					context.check_function(context.builder.table.fns[fn_name])!
+				}
+			}
 		}
-
-		mut automata := context.reference_map[identifier] or {
-			return error('Could not find the automata for ${identifier}.')
-		}
-
-		automata.accept('${context.just_name}.${call_expr.name}') or {
-			return error(serialise_state_error(err, fn_file, call_expr.pos.line_nr))
+		else {
+			// unsupported expression type
+			println('UNSUPPORTED: ${expression.type_name()}\t${expression}')
 		}
 	}
 }
