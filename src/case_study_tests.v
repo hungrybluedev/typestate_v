c5961b311f932f49745059e3a2e97bea660b01b3
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-07-28 12:17:23 +0100

    Populate cases iteratively

--- src/case_study_tests.v
+++ src/case_study_tests.v
@@ -5,41 +5,33 @@ import cli
 
 const (
 	case_study_location    = os.join_path('src', 'case_studies')
-	case_study_directories = [
-		// Read-only file
-		os.join_path('01_read_only_file', 'case01_errors'),
-		os.join_path('01_read_only_file', 'case02_warnings'),
-		os.join_path('01_read_only_file', 'case03_normal'),
-		os.join_path('01_read_only_file', 'case04_invalid'),
-		// Speaker
-		os.join_path('02_speaker', 'case01_errors'),
-		os.join_path('02_speaker', 'case02_warnings'),
-		os.join_path('02_speaker', 'case03_normal'),
-		os.join_path('02_speaker', 'case04_invalid'),
-		// Histogram
-		os.join_path('03_histogram', 'case01_errors'),
-		os.join_path('03_histogram', 'case02_warnings'),
-		os.join_path('03_histogram', 'case03_normal'),
-		os.join_path('03_histogram', 'case04_invalid'),
-		// Email API
-		os.join_path('04_email_api', 'case01_errors'),
-		os.join_path('04_email_api', 'case02_warnings'),
-		os.join_path('04_email_api', 'case03_normal'),
-		os.join_path('04_email_api', 'case04_invalid'),
-		// Fibonacci
-		os.join_path('05_fibonacci', 'case01_errors'),
-		os.join_path('05_fibonacci', 'case02_warnings'),
-		os.join_path('05_fibonacci', 'case03_normal'),
-		os.join_path('05_fibonacci', 'case04_invalid'),
-	]
+	case_study_directories = get_all_sub_cases(case_study_location) or {
+		panic('Could not obtain all the test cases.')
+	}
 )
+
+fn get_all_sub_cases(case_study_location string) ![]string {
+	mut dirs := []string{}
+
+	for example in os.ls(case_study_location)! {
+		rel_path := os.join_path(case_study_location, example)
+		if os.is_dir(rel_path) {
+			// Now list all the subdirectories.
+			dirs << os.ls(rel_path)!
+				.map(os.join_path(rel_path, it))
+				.filter(os.is_dir(it))
+		}
+	}
+
+	return dirs
+}
 
 fn run_for_case_studies(command cli.Command) ! {
 	mut count := 0
 	working_dir := os.getwd() + os.path_separator
 
 	for directory in case_study_directories {
-		path := os.join_path(os.real_path(case_study_location), directory)
+		path := os.real_path(directory)
 
 		println('\nRunning case study: typestate_v ${path}\n')
 
@@ -58,8 +50,8 @@ fn run_for_case_studies(command cli.Command) ! {
 			// Normalise to get relative paths.
 			mut lines := actual_error_raw.split_into_lines()
 			for index, line in lines {
-				if line.starts_with('\tFile: ') {
-					lines[index] = '\tFile: ${line.all_after(working_dir)}'
+				if line.contains(':') {
+					lines[index] = '${line.all_after(working_dir)}'
 				}
 			}
 			actual_error := lines.join_lines() + '\n'
