135e57e4cc2ab71bb385d352a15681e0956fc76b
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-07-28 11:52:32 +0100

    Update help string

--- src/metadata.v
+++ src/metadata.v
@@ -8,6 +8,6 @@ pub const (
 	version     = manifest.version
 	name        = manifest.name
 	description = manifest.description
-	help_string = 'A simple command line tool for managing your V modules.'
+	help_string = 'A simple command line tool for typestate checking in V.'
 	usage       = 'typestate_v [directory]\ntypestate_v help'
 )
